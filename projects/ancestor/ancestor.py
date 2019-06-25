f3ccc06df3eb9fc0781af7a1f952e32721d3b935
bcabanayan <bcabanayan@gmail.com>
2019-06-24 20:57:07 -0700

    Added psuedocode for ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,9 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+# looks like depth first search--would be traversing the graph to the farthest away ancestor
+# not technically distance, but does it relate more to the number of parents?
+# 1. start with input value
+# 2. locate the parents of the input value
+# 3. locate parents of the parents, etc. etc
+# 4. increment counts of parents
+# 5. return parent after finding the max number of parents?
